c314c6c827a7c9cee1155ab27339ec7f13c2624c
lalatendu11 <lalatendung@gmail.com>
2024-12-07 10:52:45 +0530

    Values Modifed in class

--- Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -3,19 +3,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=345,b=98,c;
+	int a=567,b=8,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=78,b=55,c;
+	int a=234,b=89,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=345,b=8,c;
+	int a=34555,b=67,c;
 	c=a+b;
 	System.out.println(c);
 }
