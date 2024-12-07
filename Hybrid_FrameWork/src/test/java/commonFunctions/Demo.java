5cf19c4dea2a74522ba70c365006e9fdcc9716e4
lalatendu11 <lalatendung@gmail.com>
2024-12-07 10:50:43 +0530

    New Classs created

---
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=345,b=98,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=78,b=55,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=345,b=8,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
